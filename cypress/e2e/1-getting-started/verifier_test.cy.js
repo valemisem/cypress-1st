ab689fb43442f369992f032be9565abb954c4725
valemisem <valentina.misem@gmail.com>
2023-12-11 21:41:10 +0100

    5.1.2

---
+++ cypress/e2e/1-getting-started/verifier_test.cy.js
@@ -0,0 +1,7 @@
+describe("Testing Verifier website", () => {
+  it("Успешная загрузка сайта sqlverifier", () => {
+    cy.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/");
+
+    cy.contains('Tasks').should('be.visible')
+  });
+});
